63a41939ca4c3f5d7e67f8dfd834a0abb9e21127
Daniel Bricola <danielbricola@hotmail.com>
2024-11-22 19:38:00 -0300

    :fire: Tasks progress

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const renderTasksProgressData = (tasks) => {
+  let tasksProgress;
+  const tasksProgressDOM = document.getElementById('tasks-progress');
+
+  if (tasksProgressDOM) tasksProgress = tasksProgressDOM;
+  else {
+    const newTasksProgressDOM = document.createElement('div');
+    newTasksProgressDOM.id = "tasks-progress";
+    document.getElementsByTagName('footer')[0].appendChild(newTasksProgressDOM);
+    tasksProgress = newTasksProgressDOM;
+  }
+
+  const doneTasks = tasks.filter(({ checked }) => checked).length
+  const totalTasks = tasks.length;
+  tasksProgress.textContent = `${doneTasks}/${totalTasks} concluídas`;
+}
+
 const getTasksFromLocalStorage = () => {
   const localTasks = JSON.parse(window.localStorage.getItem("tasks"));
   return localTasks ? localTasks : [];
@@ -13,6 +30,7 @@ const removeTask = (taskId) => {
     ({ id }) => parseInt(id) !== parseInt(taskId)
   );
   setTasksInLocalStorage(updatedTasks);
+  renderTasksProgressData(updatedTasks)
 
   document
     .getElementById("todo-list")
@@ -27,6 +45,7 @@ const removeDoneTasks = () => {
 
   const updatedTasks = tasks.filter(({ checked }) => !checked);
   setTasksInLocalStorage(updatedTasks);
+  renderTasksProgressData(updatedTasks)
 
   tasksToRemove.forEach((taskToRemove) => {
     document
@@ -65,6 +84,7 @@ const onCheckboxClick = (event) => {
   });
 
   setTasksInLocalStorage(updatedTasks);
+  renderTasksProgressData(updatedTasks)
 };
 
 const getCheckboxInput = ({ id, description, checked }) => {
@@ -128,6 +148,7 @@ const createTask = async (event) => {
     },
   ];
   setTasksInLocalStorage(updatedTasks);
+  renderTasksProgressData(updatedTasks)
 
   document.getElementById("description").value = "";
   document.getElementById("save-task").removeAttribute("disabled");
@@ -143,4 +164,6 @@ window.onload = function () {
     const checkbox = getCheckboxInput(task);
     createTaskListItem(task, checkbox);
   });
+
+  renderTasksProgressData(tasks);
 };
